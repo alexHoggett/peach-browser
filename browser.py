9cb7b7480a483bb301d693dfa0bc302f49646072
Alex <alex.shabib.hoggett@proton.me>
2025-03-31 21:11:29 +0100

    added scrolling

--- browser.py
+++ browser.py
@@ -2,6 +2,41 @@ import tkinter
 from url import URL
 
 WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+def lex(body):
+   text = ""
+   in_tag = False
+   in_entity = False
+   entity = ""
+   for c in body:
+      if c == "<":
+         in_tag = True
+      elif c == ">":
+         in_tag = False
+      elif c == "&":
+         in_entity = True
+      elif in_entity:
+         entity += c
+         if entity in ("lt;", "gt;"):
+            text += "<" if entity == "lt;" else ">"
+            in_entity = False
+            entity = ""
+      elif not in_tag:
+         text += c
+   return text
+
+def layout(text):
+   display_list = []
+   cursor_x, cursor_y = HSTEP, VSTEP
+   for c in text:
+      display_list.append((cursor_x, cursor_y, c))
+      cursor_x += HSTEP
+      if cursor_x >= WIDTH - HSTEP:
+         cursor_y += VSTEP
+         cursor_x = HSTEP
+   return display_list
 
 class Browser:
    def __init__(self):
@@ -12,12 +47,24 @@ class Browser:
          height=HEIGHT
       )
       self.canvas.pack()
+      self.scroll = 0
+      self.window.bind("<Down>", self.scrolldown)
 
    def load(self, url):
-      self.canvas.create_rectangle(10, 20, 400, 300)
-      self.canvas.create_oval(100, 100, 150, 150)
-      self.canvas.create_text(200, 150, text="Hi!")
+      text = lex(url.request())
+      self.display_list = layout(text)
+      self.draw()
 
+   def draw(self):
+      self.canvas.delete("all")
+      for x, y, c in self.display_list:
+         self.canvas.create_text(x, y - self.scroll, text=c)
+
+   def scrolldown(self, e):
+      self.scroll += SCROLL_STEP
+      self.draw()
+
+      
 if __name__ == "__main__":
    import sys
    Browser().load(URL(sys.argv[1]))
