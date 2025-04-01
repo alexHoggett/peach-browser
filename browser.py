33fc9094b5d41a5967a965255c554b2853738f8c
Alex <alex.shabib.hoggett@proton.me>
2025-04-01 21:44:57 +0100

    added paragraph support

--- browser.py
+++ browser.py
@@ -31,8 +31,13 @@ def layout(text):
    display_list = []
    cursor_x, cursor_y = HSTEP, VSTEP
    for c in text:
-      display_list.append((cursor_x, cursor_y, c))
-      cursor_x += HSTEP
+      if c == '\n':
+         cursor_y += (VSTEP * 1.1)
+         cursor_x = HSTEP
+      else:
+         display_list.append((cursor_x, cursor_y, c))
+         cursor_x += HSTEP
+
       if cursor_x >= WIDTH - HSTEP:
          cursor_y += VSTEP
          cursor_x = HSTEP
