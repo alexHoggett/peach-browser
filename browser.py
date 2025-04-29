56e63442c3809256db9bff1b3c64199b83c6ce38
Alex <alex.shabib.hoggett@proton.me>
2025-04-29 21:40:39 +0100

    loads of things

--- browser.py
+++ browser.py
@@ -57,6 +57,7 @@ class Browser:
       )
       self.canvas.pack(fill=tkinter.BOTH, expand=True)
       self.scroll = 0
+
       self.window.bind("<Down>", self.scrolldown)
       self.window.bind("<Up>", self.scrollup)
       self.window.bind("<MouseWheel>", self.scrollwheel)
@@ -68,30 +69,35 @@ class Browser:
       self.draw()
 
    def draw(self):
+      print(self.scroll)
+      # Clear the canvas
       self.canvas.delete("all")
+
+      # Draw text to the screen
       for x, y, c in self.display_list:
          if y > self.scroll + HEIGHT: continue
          if y + VSTEP < self.scroll: continue
          self.canvas.create_text(x, y - self.scroll, text=c)
+
+      # Draw thew scrollbar
+      content_height = max(y for x, y, c in self.display_list) + VSTEP
+      thumb_top = (self.scroll / content_height) * self.height
+      self.canvas.create_rectangle(self.width - 10, thumb_top, self.width, thumb_top + 40, fill="gray")
 
    def scrolldown(self, e):
       self.scroll += SCROLL_STEP
       self.draw()
 
    def scrollup(self, e):
-      if self.scroll > 0 + SCROLL_STEP:
-         self.scroll -= SCROLL_STEP
+      self.scroll = max(self.scroll - SCROLL_STEP, 0)
       self.draw()
 
    def scrollwheel(self, e):
-      self.scroll -= e.delta * 5
-      self.draw()
+      if self.scroll >= 0 - 5 and self.scroll <= HEIGHT + 5:
+         self.scroll -= e.delta * 5
+         self.draw()
 
    def resize(self, e):
-      # if self.canvas.winfo_width() == e.width and self.canvas.winfo_height() == e.height:
-      #   return  # Avoid unnecessary resizing
-      print('resizing')
-      print(e.width, e.height)
       self.width, self.height = e.width, e.height
       self.display_list = layout(self.text, self.width)
       self.draw()
@@ -99,5 +105,10 @@ class Browser:
       
 if __name__ == "__main__":
    import sys
-   Browser().load(URL(sys.argv[1]))
+   if len(sys.argv) == 1:
+      # No arguements so provide a default file
+      Browser().load(URL("https://browser.engineering/examples/xiyouji.html"))
+   else:
+      Browser().load(URL(sys.argv[1]))
+
    tkinter.mainloop()
